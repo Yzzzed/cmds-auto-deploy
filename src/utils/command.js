2b71ea927286f821c6ebe8532aa27afc47629e97
Yzzzed <hyzzzed@qq.com>
2021-11-29 14:50:27 +0800

    fix: avoid run command 'rm -rf */'

--- src/utils/command.js
+++ src/utils/command.js
@@ -1,4 +1,9 @@
 function run(ssh, command, path) {
+  // 检测执行rm -rf xxx 防止rm -rf *等
+  const rmGroup = ['/', '*', '/*', '.', '']
+  if(command.indexOf('rm -rf') > 0 && rmGroup.includes(command.split('rm -rf')[1].trim())) {
+    throw Error(`禁止使用命令: '${command}'`)
+  }
   return new Promise((resolve, reject) => {
     ssh.execCommand(command, {
       cwd: path
